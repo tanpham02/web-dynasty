ed5ac0140842dd72debae3f325adec16bba5d19f
tanpham02 <phamvantan1311@gmail.com>
2023-11-26 23:22:02 +0700

    fix bug

--- src/pages/Customer/CustomerList/CustomerTable/index.tsx
+++ src/pages/Customer/CustomerList/CustomerTable/index.tsx
@@ -1,23 +1,34 @@
-import { CalculatorOutlined, EditOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { InfiniteData } from '@tanstack/react-query';
-import { Avatar, Button, Select, Table, TablePaginationConfig } from 'antd';
-import { useMemo, useState } from 'react';
-import { Customer } from '~/models/customers';
-import { Breakpoint, ListDataResponse, ListResponse } from '~/types';
-import CreateOrUpdateCustomerModal from '../CreateOrUpdateCustomerModal';
-import moment from 'moment';
-import { DATE_FORMAT_DDMMYYYY } from '~/utils/date.utils';
-import UpdateCustomerPointModal from '../UpdateCustomerPoint';
-import customerAddressService from '~/services/customerService/customerAddressService';
-import { CustomerAddressList } from '~/models/customers/customerAddress';
-import CustomerAddressTable from './CustomerAddressTable';
+import {
+  CalculatorOutlined,
+  EditOutlined,
+  InfoCircleOutlined,
+} from "@ant-design/icons";
+import { InfiniteData } from "@tanstack/react-query";
+import {
+  Avatar,
+  Button,
+  Empty,
+  Select,
+  Table,
+  TablePaginationConfig,
+} from "antd";
+import { useMemo, useState } from "react";
+import { Customer } from "~/models/customers";
+import { Breakpoint, ListDataResponse, ListResponse } from "~/types";
+import CreateOrUpdateCustomerModal from "../CreateOrUpdateCustomerModal";
+import moment from "moment";
+import { DATE_FORMAT_DDMMYYYY } from "~/utils/date.utils";
+import UpdateCustomerPointModal from "../UpdateCustomerPoint";
+import customerAddressService from "~/services/customerService/customerAddressService";
+import { CustomerAddressList } from "~/models/customers/customerAddress";
+import CustomerAddressTable from "./CustomerAddressTable";
 
 interface TableColumn {
   title: string;
   dataIndex?: keyof Customer;
   key?: keyof Customer;
   sorter?: boolean;
-  align?: 'left' | 'center' | 'right';
+  align?: "left" | "center" | "right";
   render?: (value: any, record: Customer, index: number) => React.ReactNode;
   responsive?: Breakpoint[];
 }
@@ -35,19 +46,25 @@ export interface CustomerAddressProps {
   dataCustomerAddress?: CustomerAddressList;
 }
 
-const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTableProps) => {
-  const [showCreateOrUpdateCustomerModal, setShowCreateOrUpdateCustomerModal] = useState<{
-    isShowCreateOrUpdateCustomerModal?: boolean;
-    isShowUpdateCustomerPointModal?: boolean;
-    customerID: number;
-    type?: 'UPDATE' | 'CREATE' | 'DETAIL';
-  }>({
-    isShowCreateOrUpdateCustomerModal: false,
-    isShowUpdateCustomerPointModal: false,
-    customerID: CUSTOMER_ID_WHEN_EMPTY,
-    type: 'CREATE',
-  });
-  const [dataCustomerAddress, setDataCustomerAddress] = useState<CustomerAddressProps>({});
+const CustomerTable = ({
+  data,
+  refreshData,
+  handleTableChange,
+}: CustomerTableProps) => {
+  const [showCreateOrUpdateCustomerModal, setShowCreateOrUpdateCustomerModal] =
+    useState<{
+      isShowCreateOrUpdateCustomerModal?: boolean;
+      isShowUpdateCustomerPointModal?: boolean;
+      customerID: number;
+      type?: "UPDATE" | "CREATE" | "DETAIL";
+    }>({
+      isShowCreateOrUpdateCustomerModal: false,
+      isShowUpdateCustomerPointModal: false,
+      customerID: CUSTOMER_ID_WHEN_EMPTY,
+      type: "CREATE",
+    });
+  const [dataCustomerAddress, setDataCustomerAddress] =
+    useState<CustomerAddressProps>({});
 
   const pagination = useMemo(() => {
     const current = data?.pageIndex;
@@ -62,45 +79,50 @@ const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTablePr
 
   const COLUMNS: TableColumn[] = [
     {
-      title: 'Họ tên',
-      dataIndex: 'fullName',
-      key: 'fullName',
-      align: 'center',
+      title: "Họ tên",
+      dataIndex: "fullName",
+      key: "fullName",
+      align: "center",
     },
     {
-      title: 'Số điện thoại',
-      dataIndex: 'phoneNumber',
-      key: 'phoneNumber',
-      align: 'center',
+      title: "Số điện thoại",
+      dataIndex: "phoneNumber",
+      key: "phoneNumber",
+      align: "center",
     },
     {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-      align: 'center',
-      responsive: ['lg'],
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      align: "center",
+      responsive: ["lg"],
     },
     {
-      title: 'Ngày sinh',
-      dataIndex: 'birthday',
-      key: 'birthday',
-      render: (__index, record) => <div>{record.birthday && moment(record.birthday).format(DATE_FORMAT_DDMMYYYY)}</div>,
-      align: 'center',
-      responsive: ['md'],
+      title: "Ngày sinh",
+      dataIndex: "birthday",
+      key: "birthday",
+      render: (__index, record) => (
+        <div>
+          {record.birthday &&
+            moment(record.birthday).format(DATE_FORMAT_DDMMYYYY)}
+        </div>
+      ),
+      align: "center",
+      responsive: ["md"],
     },
     {
-      title: 'Sổ địa chỉ',
-      dataIndex: 'customerAddressId',
-      key: 'customerAddressId',
+      title: "Sổ địa chỉ",
+      dataIndex: "customerAddressId",
+      key: "customerAddressId",
       render: (__index, record) => (
         <button
-          className='py-2 px-3 bg-gray text-primary rounded-md font-semibold hover:text-white duration-200 ease-linear hover:bg-primary'
+          className="py-2 px-3 bg-gray text-primary rounded-md font-semibold hover:text-white duration-200 ease-linear hover:bg-primary"
           onClick={() => handleShowListCustomerAddress(record?._id)}
         >
           Xem sổ địa chỉ
         </button>
       ),
-      align: 'center',
+      align: "center",
     },
 
     // {
@@ -116,8 +138,14 @@ const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTablePr
   const handleShowListCustomerAddress = async (customerId?: string) => {
     try {
       if (customerId) {
-        const listCustomerAddress = await customerAddressService.getListCustomerAddressByCustomerId(customerId);
-        return setDataCustomerAddress({ visible: true, dataCustomerAddress: listCustomerAddress });
+        const listCustomerAddress =
+          await customerAddressService.getListCustomerAddressByCustomerId(
+            customerId
+          );
+        return setDataCustomerAddress({
+          visible: true,
+          dataCustomerAddress: listCustomerAddress,
+        });
       }
       setDataCustomerAddress({ visible: false });
     } catch (error) {
@@ -126,24 +154,30 @@ const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTablePr
   };
 
   const handleConvertAddress = (record: Customer) => {
-    const addressParts = [record.address, record.ward, record.district, record.city];
-    return addressParts.filter(Boolean).join(', ');
+    const addressParts = [
+      record.address,
+      record.ward,
+      record.district,
+      record.city,
+    ];
+    return addressParts.filter(Boolean).join(", ");
   };
 
   const handleCloseProductDetailModal = () => {
     setShowCreateOrUpdateCustomerModal({
       isShowCreateOrUpdateCustomerModal: false,
       customerID: CUSTOMER_ID_WHEN_EMPTY,
-      type: 'CREATE',
+      type: "CREATE",
     });
   };
 
   const handleShowCustomerDetail = (customerID: number | undefined) => {
     if (customerID) {
       setShowCreateOrUpdateCustomerModal({
-        isShowCreateOrUpdateCustomerModal: !showCreateOrUpdateCustomerModal.isShowCreateOrUpdateCustomerModal,
+        isShowCreateOrUpdateCustomerModal:
+          !showCreateOrUpdateCustomerModal.isShowCreateOrUpdateCustomerModal,
         customerID: customerID,
-        type: 'DETAIL',
+        type: "DETAIL",
       });
     }
   };
@@ -151,9 +185,10 @@ const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTablePr
   const handleShowCustomerUpdateForm = (customerID: number | undefined) => {
     if (customerID) {
       setShowCreateOrUpdateCustomerModal({
-        isShowCreateOrUpdateCustomerModal: !showCreateOrUpdateCustomerModal.isShowCreateOrUpdateCustomerModal,
+        isShowCreateOrUpdateCustomerModal:
+          !showCreateOrUpdateCustomerModal.isShowCreateOrUpdateCustomerModal,
         customerID: customerID,
-        type: 'UPDATE',
+        type: "UPDATE",
       });
     }
   };
@@ -165,10 +200,13 @@ const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTablePr
     });
   };
 
-  const handleShowUpdateCustomerPointModal = (customerID: number | undefined) => {
+  const handleShowUpdateCustomerPointModal = (
+    customerID: number | undefined
+  ) => {
     if (customerID) {
       setShowCreateOrUpdateCustomerModal({
-        isShowUpdateCustomerPointModal: !showCreateOrUpdateCustomerModal.isShowUpdateCustomerPointModal,
+        isShowUpdateCustomerPointModal:
+          !showCreateOrUpdateCustomerModal.isShowUpdateCustomerPointModal,
         customerID: customerID,
       });
     }
@@ -177,23 +215,33 @@ const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTablePr
   return (
     <>
       <Table
-        rowKey='_id'
+        rowKey="_id"
         dataSource={data?.data}
         columns={COLUMNS}
-        className='rounded-sm border border-stroke w-full max-w-full bg-white pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1'
-        rowClassName='text-black dark:text-white'
+        className="rounded-sm border border-stroke w-full max-w-full bg-white pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1"
+        rowClassName="text-black dark:text-white"
         onChange={handleTableChange}
         pagination={{
           current: pagination.pageCurrent,
           pageSize: pagination.pageSize,
           total: pagination.totalElements,
         }}
+        locale={{
+          emptyText: (
+            <Empty
+              image={Empty.PRESENTED_IMAGE_SIMPLE}
+              description="Không có dữ liệu"
+            />
+          ),
+        }}
       />
       {showCreateOrUpdateCustomerModal.isShowCreateOrUpdateCustomerModal && (
         <CreateOrUpdateCustomerModal
-          isShowCustomerModal={showCreateOrUpdateCustomerModal.isShowCreateOrUpdateCustomerModal}
+          isShowCustomerModal={
+            showCreateOrUpdateCustomerModal.isShowCreateOrUpdateCustomerModal
+          }
           customerID={showCreateOrUpdateCustomerModal.customerID}
-          type={showCreateOrUpdateCustomerModal.type || 'DETAIL'}
+          type={showCreateOrUpdateCustomerModal.type || "DETAIL"}
           handleCancelModal={handleCloseProductDetailModal}
           handleConfirmModal={handleCloseProductDetailModal}
           refreshData={refreshData}
@@ -201,7 +249,9 @@ const CustomerTable = ({ data, refreshData, handleTableChange }: CustomerTablePr
       )}
       {showCreateOrUpdateCustomerModal.isShowUpdateCustomerPointModal && (
         <UpdateCustomerPointModal
-          isShowUpdateCustomerPointModal={showCreateOrUpdateCustomerModal.isShowUpdateCustomerPointModal}
+          isShowUpdateCustomerPointModal={
+            showCreateOrUpdateCustomerModal.isShowUpdateCustomerPointModal
+          }
           customerID={showCreateOrUpdateCustomerModal.customerID}
           handleCancelModal={handleCloseUpdateCustomerPointModal}
           handleConfirmModal={handleCloseUpdateCustomerPointModal}
