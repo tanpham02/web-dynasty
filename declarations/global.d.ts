a2c83d2c431b32e6ec4dd3167d6266b0d820ee61
Tan_Pham <tan_pham@datahouse.com>
2024-11-16 12:44:55 +0700

    deploy production 2024-16-11

--- declarations/global.d.ts
+++ declarations/global.d.ts
@@ -1,36 +1,36 @@
 declare module '*.svg' {
-  const content: string;
-  export default content;
+  const content: string
+  export default content
 }
 declare module '*.jpg' {
-  const content: string;
-  export default content;
+  const content: string
+  export default content
 }
 
 declare module '*.gif' {
-  const content: string;
-  export default content;
+  const content: string
+  export default content
 }
 
 declare module '*.png' {
-  const content: string;
-  export default content;
+  const content: string
+  export default content
 }
 
 declare module '*.json' {
-  const content: string;
-  export default content;
+  const content: string
+  export default content
 }
 
 declare module '*.webp' {
-  const content: string;
-  export default content;
+  const content: string
+  export default content
 }
 
-declare module 'jsvectormap';
+declare module 'jsvectormap'
 
 declare module 'react-google-maps' {
   interface GoogleMapProps {
-    children?: ReactNode | undefined;
+    children?: ReactNode | undefined
   }
 }
