8e0324a36351d5cc6b94be7c5b87ee3bf11be24d
Tan_Pham <tan_pham@datahouse.com>
2024-11-17 12:54:55 +0700

    fix: don't refill username and pw when login

--- src/pages/AuthenticationPage/components/LoginForm/useLoginForm.ts
+++ src/pages/AuthenticationPage/components/LoginForm/useLoginForm.ts
@@ -1,54 +1,54 @@
-import { useSnackbar } from 'notistack';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { LOCAL_STORAGE, PATH_NAME } from '~/constants';
+import { useSnackbar } from 'notistack'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
-import { SignInType } from '~/models';
-import { AuthService } from '~/services';
+import { LOCAL_STORAGE, PATH_NAME } from '~/constants'
+import { SignInType } from '~/models'
+import { AuthService } from '~/services'
 
 export const useLoginForm = () => {
-  const navigation = useNavigate();
-  const { enqueueSnackbar } = useSnackbar();
+  const navigation = useNavigate()
+  const { enqueueSnackbar } = useSnackbar()
 
   const formMethods = useForm<SignInType>({
     defaultValues: {
-      username: 'admin',
-      password: '13112002',
+      username: '',
+      password: '',
     },
-  });
+  })
 
   const {
     handleSubmit,
     reset,
     formState: { isSubmitting: isLogin },
-  } = formMethods;
+  } = formMethods
 
   const login = async (data: SignInType) => {
     try {
       const dataSubmit: SignInType = {
         username: data.username,
         password: data.password,
-      };
+      }
 
-      const response = await AuthService.signIn(dataSubmit);
+      const response = await AuthService.signIn(dataSubmit)
 
-      localStorage.setItem(LOCAL_STORAGE.ACCESS_TOKEN, response.accessToken);
-      localStorage.setItem(LOCAL_STORAGE.REFRESH_TOKEN, response.refreshToken);
-      navigation(PATH_NAME.STAFF_MANAGEMENT);
+      localStorage.setItem(LOCAL_STORAGE.ACCESS_TOKEN, response.accessToken)
+      localStorage.setItem(LOCAL_STORAGE.REFRESH_TOKEN, response.refreshToken)
+      navigation(PATH_NAME.STAFF_MANAGEMENT)
       enqueueSnackbar({
         message: 'Đăng nhập thành công!',
-      });
-      reset();
+      })
+      reset()
     } catch (error) {
       enqueueSnackbar({
         message: 'Đăng nhập thất bại!',
         variant: 'error',
-      });
-      console.log(error);
+      })
+      console.log(error)
     }
-  };
+  }
 
-  const handleLogin = handleSubmit(login);
+  const handleLogin = handleSubmit(login)
 
-  return { handleLogin, formMethods, isLogin } as const;
-};
+  return { handleLogin, formMethods, isLogin } as const
+}
