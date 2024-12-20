0c90a7df2c256451bbd1a30ccf5ad1c8cecca696
Tan_Pham <tan_pham@datahouse.com>
2024-12-20 08:38:13 +0700

    deploy production 2024-12-20

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,13 +17,17 @@ export default defineConfig({
     port: 1311, // you can replace this port with any port
   },
 
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1MB
+  },
+
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src"),
-      "~ assets": path.resolve(__dirname, "src/assets"),
-      "~ components": path.resolve(__dirname, "src/components"),
-      "~ routers": path.resolve(__dirname, "src/routers"),
-      "~ layout": path.resolve(__dirname, "src/layout"),
+      '~': path.resolve(__dirname, './src'),
+      '~ assets': path.resolve(__dirname, 'src/assets'),
+      '~ components': path.resolve(__dirname, 'src/components'),
+      '~ routers': path.resolve(__dirname, 'src/routers'),
+      '~ layout': path.resolve(__dirname, 'src/layout'),
     },
   },
-});
+})
